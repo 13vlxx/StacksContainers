dd38ef3cfe991aa07c895dc4f88c413b536d87c6
ALX <alexmonac13@gmail.com>
2023-07-17 21:28:55 +0200

    Initial Commit

---
+++ StacksContainers/StacksContainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksContainersApp.swift
+//  StacksContainers
+//
+//  Created by Alex on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StacksContainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
